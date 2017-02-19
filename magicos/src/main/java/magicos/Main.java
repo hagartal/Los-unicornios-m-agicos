7c82dec5067307f8e739080905fdb8e445cce9e9
Noa <Noa>
2017-02-19 22:12:42 +0200

    Basic Maven Project

---
+++ magicos/src/main/java/magicos/Main.java
@@ -0,0 +1,15 @@
+package magicos;
+
+/**
+ * Created by Noa Agiv on 2/19/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+    public static int echoFunc(int x){
+        return x;
+    }
+}
---
+++ magicos/src/test/java/magicos/MainTest.java
@@ -0,0 +1,16 @@
+package magicos; /**
+ * Created by Noa Agiv on 2/19/2017.
+ */
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest extends TestCase {
+
+    @Test
+    public void test (){
+        int y = Main.echoFunc(3);
+        Assert.assertEquals(3, y);
+    }
+
+}
